2bd85109bf826c57564da888263af35ae50e2e46
Marius Myburg <mariusmyburg@gmail.com>
2012-06-17 21:31:59 +0200

    CRand and CCent class.

--- POSMath/money.h
+++ POSMath/money.h
@@ -1,29 +1,72 @@
 
-class CMoney {
+#define TEN_THOUSAND 10000.0
+
+class CRand
+{
 private:
-	long m_Rands;
+	int m_Value;
 
 public:
-	CMoney(long Rands)
+	CRand(int value) {
+		m_Value = value;
+	}
+};
+
+class CCent
+{
+private:
+	int m_Value;
+};
+
+
+
+class CMoney {
+private:
+	double m_MoneyValueTimesTenThousand;
+
+public:
+	CMoney(double Rands)
 	{
-		m_Rands = Rands;
+		m_MoneyValueTimesTenThousand = (Rands * TEN_THOUSAND);
 	}
 
 	CMoney(int Rands, int Cents)
 	{
+		double r = Rands;
+		double c = Cents;
+		r += (c/100.0);
+		m_MoneyValueTimesTenThousand = (r * TEN_THOUSAND);
 	}
 
 	void operator *=(int scale)
 	{
-		m_Rands = m_Rands * scale;
+		m_MoneyValueTimesTenThousand = m_MoneyValueTimesTenThousand * scale;
 	}
 
 	CMoney& operator *(CMoney right)
 	{
 		CMoney ret(0);
-		ret = m_Rands * right.m_Rands;
+		ret = m_MoneyValueTimesTenThousand * right.m_MoneyValueTimesTenThousand;
 
 		return ret;
+	}
+
+
+	CRand GetRands()
+	{
+		double v = m_MoneyValueTimesTenThousand;
+		v /= TEN_THOUSAND;
+		int rands = (int)v;
+		return CRand(rands);
+	}
+
+	int GetCents()
+	{
+		double v = m_MoneyValueTimesTenThousand;
+		v /= TEN_THOUSAND;
+		double rands = (int)v;
+		double cents = (v-rands);
+		return (int)(cents * 100.0);
 	}
 
 };
--- POSMath/moneytest.cpp
+++ POSMath/moneytest.cpp
@@ -2,6 +2,9 @@
 
 void main()
 {
-	CMoney m1(100);
+	CMoney m1(100, 50);
 	CMoney m2 = m1 * m1;
+
+	auto rands = m1.GetRands();
+	auto cents = m1.GetCents();
 }
